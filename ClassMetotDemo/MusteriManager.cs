900c2632d04bab21cb8557319034bee087ea0a49
TalhaEreli <70755170+TalhaEreli@users.noreply.github.com>
2021-01-30 14:23:20 +0300

    ClassMetotDemo
    ClassMetotDemo

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Sisteme Eklendi" + " " + musteri.Name + " " + musteri.LastName);
+        }
+
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Sistemden Silindi " + " " + musteri.Name);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri()
+            {
+                Id = 01,
+                Name = "Leyla",
+                LastName = "Gülen",
+                Email = "leylagülen@gmail.com",
+                Password = 86495
+            };
+
+
+            Musteri musteri2 = new Musteri()
+            {
+                Id = 02,
+                Name = "Ayşe",
+                LastName = "Diken",
+                Email = "aysediken@gmail.com",
+                Password = 45764
+            };
+
+
+            Musteri musteri3 = new Musteri()
+            {
+                Id = 03,
+                Name = "Ahmet",
+                LastName = "Yıldız",
+                Email = "ahmetyıldız@gmail.com",
+                Password = 14548
+            };
+            
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine("Musteri Id : " + musteri.Id);
+                Console.WriteLine("Musteri Adı : " + musteri.Name);
+                Console.WriteLine("Musteri Soyadı : " + musteri.LastName);
+                Console.WriteLine("Musteri Maili : " + musteri.Email);
+                Console.WriteLine("Musteri Şifresi : " + musteri.Password);
+            }
+
+            MusteriManager musteriManager = new MusteriManager();
+            Console.WriteLine("-----------------");
+            musteriManager.MusteriEkle(musteri1);
+            Console.WriteLine("-----------------");
+            musteriManager.MusteriEkle(musteri2);
+            Console.WriteLine("-----------------");
+            musteriManager.MusteriEkle(musteri3);
+          
+
+        }
+    }
+}
